221892eefa19b3a51a0d47a6e9a75f25669772bb
thecoocko <butenkonaddezhda@gmail.com>
2023-07-05 15:20:16 +0200

    done:splitter

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,7 +1,9 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class StringSplitter {
 
@@ -12,7 +14,15 @@ public class StringSplitter {
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
+
+
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String regex  = String.join("|",delimiters.toArray(new String[0]));
+        StringTokenizer st = new StringTokenizer(source,regex);
+        List<String> result = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            result.add(st.nextToken());
+        }
+        return result;
     }
 }
